76e08f7edd23be0a40cb39362c2183a7d03d1540
suryapugal22 <u.pugal@gmail.com>
2020-02-01 11:02:00 +0530

    index.js updated

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 //app.listen(PORT);
 
